612c908d36b1390ec46a726043f94c20a39ac033
noahpro <noahpro@gmail.com>
2023-07-16 17:37:42 -0400

    compose up

--- backend/core/celery_app.py
+++ backend/core/celery_app.py
@@ -7,10 +7,10 @@ celery_app.conf.task_routes = {"worker.test_celery": "main-queue"}
 import worker  # noqa
 
 
-@celery_app.on_after_configure.connect  # type: ignore
-def setup_periodic_tasks(sender, **kwargs):
-    sender.add_periodic_task(
-        crontab(minute="*/1"),
-        worker.test_celery.s("hello"),
-        name="test every minute",
-    )
+# @celery_app.on_after_configure.connect  # type: ignore
+# def setup_periodic_tasks(sender, **kwargs):
+#     sender.add_periodic_task(
+#         crontab(minute="*/1"),
+#         worker.test_celery.s("hello"),
+#         name="test every minute",
+#     )
